bc1deae485344e7bd25b5a3c59f9180f52eda372
thien <ducthien8698@gmail.com>
2022-03-09 20:08:50 +0700

    update image 8:08 9/3/2021

--- Assets/DATA Plus/Spries/EnemySuicide.cs
+++ Assets/DATA Plus/Spries/EnemySuicide.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(EnemyHealth))]
+//[RequireComponent(typeof(EnemyHealth))]
 public class EnemySuicide : MonoBehaviour
 {
     public int damage;
